8aea91655236b36e66fd53c188ef1fb5a7763a68
Gabriel Ribeiro <gabrielderibeiroo@gmail.com>
2022-10-19 17:46:08 -0300

    Guardando as letras em um array

---
+++ JogoForca/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace JogoForca
+{
+    internal class Program
+    {
+        static string[] palavraCerta;
+        static string[] teste;
+        static string letraUsuario;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("palavra");
+
+            string palavra = Console.ReadLine();
+
+            palavraCerta = new string[palavra.Length];
+
+            teste = new string[palavraCerta.Length];
+
+            for (int i = 0; i < palavraCerta.Length; i++)
+            {
+                teste.SetValue(palavra.Substring(i,1),i);
+                Console.WriteLine($" cada letra da palavra {teste[i]}");
+            }
+        }
+    }
+}
