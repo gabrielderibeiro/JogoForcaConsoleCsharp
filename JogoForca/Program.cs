60b4ae0d9cb997d097d4a4b0ac36a86bd63d4b68
Gabriel Ribeiro <gabrielderibeiroo@gmail.com>
2022-10-25 15:14:17 -0300

    Mecanica principal finalizada

--- JogoForca/Program.cs
+++ JogoForca/Program.cs
@@ -4,25 +4,67 @@ namespace JogoForca
 {
     internal class Program
     {
-        static string[] palavraCerta;
-        static string[] teste;
-        static string letraUsuario;
-
-
         static void Main(string[] args)
         {
-            Console.WriteLine("palavra");
+            List<string> letrasPalavra = new List<string>();
+            string palavraSecreta = "banana".ToUpper();
+            string chuteUsuario;
 
-            string palavra = Console.ReadLine();
-
-            palavraCerta = new string[palavra.Length];
-
-            teste = new string[palavraCerta.Length];
-
-            for (int i = 0; i < palavraCerta.Length; i++)
+            for (int i = 0; i < palavraSecreta.Length; i++)
             {
-                teste.SetValue(palavra.Substring(i,1),i);
-                Console.WriteLine($" cada letra da palavra {teste[i]}");
+                letrasPalavra.Add("_ ");
+            }
+
+            foreach (var letras in letrasPalavra)
+            {
+                Console.Write(letras);
+            }
+
+            bool perdeu = false;
+            bool acertou = false;
+            int tentativas = 0;
+
+            while (!acertou && !perdeu)
+            {
+                Console.Write("\n digite uma letra \n");
+                chuteUsuario = Console.ReadLine().ToUpper();
+
+                if (palavraSecreta.Contains(chuteUsuario))
+                {
+                    for (int i = 0; i < palavraSecreta.Length; i++)
+                    {
+                        if (palavraSecreta[i].ToString() == chuteUsuario)
+                        {
+                            letrasPalavra[i] = chuteUsuario;
+                        }
+                    }
+                }
+                else
+                {
+                    tentativas++;
+                    Console.WriteLine(tentativas);
+                }
+
+                foreach (var letras in letrasPalavra)
+                {
+                    Console.Write(letras);
+
+                }
+
+                Console.WriteLine();
+
+                if (!letrasPalavra.Contains("_ "))
+                {
+                    Console.WriteLine("ganhou o jogo");
+                    acertou = true;
+
+                }
+
+                perdeu = tentativas == 6;
+            }
+            if (perdeu)
+            {
+                Console.WriteLine("vc perdeu todas as chances");
             }
         }
     }
